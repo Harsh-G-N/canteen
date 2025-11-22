5841128872c711c4b5ae9d7746616374a95851fd
Harsh-G-N <harshnichal08@gmail.com>
2025-11-22 15:42:51 +0530

    Add Flask-SocketIO and Eventlet installation to build script

--- canteen-backend/build.sh
+++ canteen-backend/build.sh
@@ -5,5 +5,7 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+pip install flask-socketio eventlet
+
 # CHANGED: Execute the new script instead of the one-liner
 python create_db.py
